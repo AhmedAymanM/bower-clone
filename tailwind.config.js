3f715391a9cbcd4bbc7dfa844d20d5031ab7b883
Ahmed Ayman <ahmed.ayman93@gmail.com>
2024-07-12 16:35:39 +0200

    chore: config tailwind default color theme

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,8 +1,34 @@
 /** @type {import('tailwindcss').Config} */
 export default {
   content: ['./index.html', './src/**/*.{js,ts,jsx,tsx}'],
+
   theme: {
     extend: {},
+    colors: {
+      transparent: 'transparent',
+      current: 'currentColor',
+
+      black: {
+        DEFAULT: '#000000',
+        accent: '#223C4C',
+      },
+      white: {
+        DEFAULT: '#FFFFFF',
+        accent: '#F7F8F8',
+      },
+
+      green: '#2baf2b',
+
+      gray: '#ccc',
+      header: {
+        bg: '#ffcc2f',
+        text: '#543729',
+      },
+      blue: {
+        light: '#00acee',
+        DEFAULT: '#008ec4',
+      },
+    },
   },
   plugins: [],
 }
