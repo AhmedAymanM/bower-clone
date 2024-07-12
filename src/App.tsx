4b950bbfbd52aadf5f195ab8f0042037225fd4a1
Ahmed Ayman <ahmed.ayman93@gmail.com>
2024-07-12 16:21:13 +0200

    feat: create content layout

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,16 @@
 import type { FC } from 'react'
 import { Header } from '@/components/features/header'
+import { ContentLayout } from '@/components/layouts/content-layout'
 
 export const App: FC = () => {
-  return <Header />
-}
+  return (
+    <>
+      <Header />
 
+      <ContentLayout className="my-4">
+        <div>sidebar</div>
+        <div>content</div>
+      </ContentLayout>
+    </>
+  )
+}
---
+++ src/components/layouts/content-layout.tsx
@@ -0,0 +1,19 @@
+import type { ReactNode } from 'react'
+
+import { cn, type ClassValue } from '@/utils/cn'
+import { Container } from '@/components/layouts/container'
+
+type ContentLayoutProps = {
+  children: ReactNode
+  className?: ClassValue
+}
+
+export const ContentLayout = ({ children, className }: ContentLayoutProps) => {
+  return (
+    <Container
+      className={cn('flex min-h-screen flex-col gap-4 md:flex-row', className)}
+    >
+      {children}
+    </Container>
+  )
+}
