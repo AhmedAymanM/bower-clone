6f16c0048ef71366b4daadcb82bb9e95694d4472
Ahmed Ayman <ahmed.ayman93@gmail.com>
2024-07-15 14:05:03 +0200

    Fix: fix crash when project repo is not valid url

--- src/components/features/project-card.tsx
+++ src/components/features/project-card.tsx
@@ -4,23 +4,29 @@ import type { Project } from '@/types/api'
 import { type ClassValue, cn } from '@/utils/cn'
 import { getFormattedCount } from '@/utils/getFormattedCount'
 import { getOwnerFromRepoUrl } from '@/utils/getOwnerFromRepoUrl'
+import { isValidUrl as checkIsValidUrl } from '@/utils/isValidUrl'
 
 type ProjectCardProps = {
   project: Project
 }
 
 const ProjectCard = ({ project }: ProjectCardProps) => {
+  const isValidUrl = checkIsValidUrl(project.repository_url)
   return (
     <a
       className="flex max-h-fit min-h-28 w-full flex-col overflow-hidden rounded-md border border-gray bg-white hover:border-blue-light"
-      href={project.repository_url}
+      href={isValidUrl ? project.repository_url : '#'}
       target="_blank"
       rel="noopener"
     >
       <div className="flex flex-wrap items-center justify-between bg-white-accent p-2">
         <div className="flex items-baseline gap-1 truncate">
           <h2 className="pb-0.5 text-lg font-bold">{project.name}</h2>
-          <Subtitle>by {getOwnerFromRepoUrl(project.repository_url)}</Subtitle>
+          {isValidUrl && (
+            <Subtitle>
+              by {getOwnerFromRepoUrl(project.repository_url)}
+            </Subtitle>
+          )}
         </div>
         <div className="flex items-center gap-1.5">
           <Subtitle>
@@ -34,8 +40,9 @@ const ProjectCard = ({ project }: ProjectCardProps) => {
           </Subtitle>
         </div>
       </div>
-
-      <p className="my-auto p-2">{project.description}</p>
+      <div className="my-auto p-2">
+        <p className="line-clamp-3">{project.description}</p>
+      </div>
     </a>
   )
 }
@@ -56,7 +63,7 @@ const Placeholder = () => {
       <div className="flex flex-wrap items-center justify-between bg-white-accent p-2">
         <div className="h-4 w-1/4 bg-gray/40" />
 
-        <div className="h-4 w-8 rounded bg-gray/40"></div>
+        <div className="h-4 w-12 rounded bg-gray/40"></div>
       </div>
       <div className="my-auto p-2">
         <div className="h-4 w-full rounded bg-gray/40"></div>
